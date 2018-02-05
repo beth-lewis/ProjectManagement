1e328ee568cb1c5f3b686433e84e19a4609170de
maxamillls <morrm046@gmail.com>
2018-02-05 16:59:38 +0000

    Test code from laptop

--- ClassLibrary/clsContracts.cs
+++ ClassLibrary/clsContracts.cs
@@ -8,5 +8,9 @@ namespace PhonePalClassLibrary
 {
     public class clsContract
     {
+        public string ContractType { get; set; }
+        public string DataAllowance { get; set; }
+        public string NumberOfMinutes { get; set; }
+        public string NumberOfTexts { get; set; }
     }
 }
--- PhonePalTest/tstContract.cs
+++ PhonePalTest/tstContract.cs
@@ -1,14 +1,59 @@
 ﻿using System;
+using PhonePalClassLibrary
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
 namespace PhonePalTest
 {
     [TestClass]
     public class tstContract
     {
+        //create an instance of the class we want to create
+        clsContract AContract = new clsContract();
         [TestMethod]
-        public void TestMethod1()
+        public void InstanceOK()
         {
+
+            //test to see if that exists
+            Assert.IsNotNull(AContract);
+        }
+        [TestMethod]
+        public void ContractTypePropertyOK()
+        {
+            //create some test data to assign to the property
+            string SomeContractType = "Pay As You Go";
+            //assign data to the property
+            AContract.ContractType = SomeContractType;
+            //test to see that the 2 values are the same
+            Assert.AreEqual(AContract.ContractType, SomeContractType);
+        }
+        [TestMethod]
+        public void DataAllowancePropertyOK()
+        {
+            //create some test data to assign to the property
+            string SomeDataAllowance = "5GB";
+            //assign data to the property
+            AContract.DataAllowance = SomeDataAllowance;
+            //test to see that the 2 values are the same
+            Assert.AreEqual(AContract.DataAllowance, SomeDataAllowance);
+        }
+        [TestMethod]
+        public void NumberOfMinutesPropertyOK()
+        {
+            //create some test data to assign to the property
+            string SomeNumberOfMinutes = "600 Mins";
+            //assign data to the property
+            AContract.NumberOfMinutes = SomeNumberOfMinutes;
+            //test to see that the 2 values are the same
+            Assert.AreEqual(AContract.NumberOfMinutes, SomeNumberOfMinutes);
+        }
+        [TestMethod]
+        public void NumberOfTextsPropertyOK()
+        {
+            //create some test data to assign to the property
+            string SomeNumberOfTexts = "Unlimited";
+            //assign data to the property
+            AContract.NumberOfTexts = SomeNumberOfTexts;
+            //test to see that the 2 values are the same
+            Assert.AreEqual(AContract.NumberOfTexts, SomeNumberOfTexts);
         }
     }
 }
