e2262986499e7ee445ddbb15e5fc13b2aed24dc4
maxamillls <morrm046@gmail.com>
2018-02-05 18:12:30 +0000

    Added The Contract Properties

--- ClassLibrary/clsContracts.cs
+++ ClassLibrary/clsContracts.cs
@@ -8,9 +8,16 @@ namespace PhonePalClassLibrary
 {
     public class clsContract
     {
-        public string ContractType { get; set; }
-        public string DataAllowance { get; set; }
-        public string NumberOfMinutes { get; set; }
-        public string NumberOfTexts { get; set; }
+        public string ContractType { get; set; }//property for Contract Type - data type string
+        public string DataAllowance { get; set; }//public property for Data Allowance - data type string
+        public string NumberOfMinutes { get; set; }//public property for Number of Minutes - data type string
+        public string NumberOfTexts { get; set; }//public property for Number of Texts - data type string
+        public decimal PricePerMonth { get; set; }//public property for Price Per Month - data type decimal
+        public int ContractNo { get; set; }
+        public int CustomerNo { get; set; }
+        public int ManufacturerNo { get; set; }
+        public string Duration { get; set; }
+        public int StaffNo { get; set; }
+        public DateTime StartDate { get; set; }
     }
 }
--- PhonePalTest/tstContract.cs
+++ PhonePalTest/tstContract.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using PhonePalClassLibrary
+using PhonePalClassLibrary;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace PhonePalTest
 {
@@ -19,41 +19,111 @@ namespace PhonePalTest
         public void ContractTypePropertyOK()
         {
             //create some test data to assign to the property
-            string SomeContractType = "Pay As You Go";
+            string TestData = "Pay As You Go";
             //assign data to the property
-            AContract.ContractType = SomeContractType;
+            AContract.ContractType = TestData;
             //test to see that the 2 values are the same
-            Assert.AreEqual(AContract.ContractType, SomeContractType);
+            Assert.AreEqual(AContract.ContractType, TestData);
         }
         [TestMethod]
         public void DataAllowancePropertyOK()
         {
             //create some test data to assign to the property
-            string SomeDataAllowance = "5GB";
+            string TestData = "5GB";
             //assign data to the property
-            AContract.DataAllowance = SomeDataAllowance;
+            AContract.DataAllowance = TestData;
             //test to see that the 2 values are the same
-            Assert.AreEqual(AContract.DataAllowance, SomeDataAllowance);
+            Assert.AreEqual(AContract.DataAllowance, TestData);
         }
         [TestMethod]
         public void NumberOfMinutesPropertyOK()
         {
             //create some test data to assign to the property
-            string SomeNumberOfMinutes = "600 Mins";
+            string TestData = "600 Mins";
             //assign data to the property
-            AContract.NumberOfMinutes = SomeNumberOfMinutes;
+            AContract.NumberOfMinutes = TestData;
             //test to see that the 2 values are the same
-            Assert.AreEqual(AContract.NumberOfMinutes, SomeNumberOfMinutes);
+            Assert.AreEqual(AContract.NumberOfMinutes, TestData);
         }
         [TestMethod]
         public void NumberOfTextsPropertyOK()
         {
             //create some test data to assign to the property
-            string SomeNumberOfTexts = "Unlimited";
+            string TestData = "Unlimited";
             //assign data to the property
-            AContract.NumberOfTexts = SomeNumberOfTexts;
+            AContract.NumberOfTexts = TestData;
             //test to see that the 2 values are the same
-            Assert.AreEqual(AContract.NumberOfTexts, SomeNumberOfTexts);
+            Assert.AreEqual(AContract.NumberOfTexts, TestData);
+        }
+        [TestMethod]
+        public void PricePerMonthPropertyOK()
+        {
+            //create some test data to assign to the property
+            decimal TestData = 30;
+            //assign data to the property
+            AContract.PricePerMonth = TestData;
+            //test to see that the 2 values are the same
+            Assert.AreEqual(AContract.PricePerMonth, TestData);
+        }
+        [TestMethod]
+        public void DurationPropertyOK()
+        {
+            //create some test data to assign to the property
+            string TestData = "12 Months";
+            //assign data to the property
+            AContract.Duration = TestData;
+            //test to see that the 2 values are the same
+            Assert.AreEqual(AContract.Duration, TestData);
+        }
+        [TestMethod]
+        public void ContractNoPropertyOK()
+        {
+            //create some test data to assign to the property
+            Int32 TestData = 1;
+            //assign data to the property
+            AContract.ContractNo = TestData;
+            //test to see that the 2 values are the same
+            Assert.AreEqual(AContract.ContractNo, TestData);
+        }
+        [TestMethod]
+        public void CustomerNoPropertyOK()
+        {
+            //create some test data to assign to the property
+            Int32 TestData = 1;
+            //assign data to the property
+            AContract.CustomerNo = TestData;
+            //test to see that the 2 values are the same
+            Assert.AreEqual(AContract.CustomerNo, TestData);
+        }
+        [TestMethod]
+        public void ManufacturerNoPropertyOK()
+        {
+            //create some test data to assign to the property
+            Int32 TestData = 1;
+            //assign data to the property
+            AContract.ManufacturerNo = TestData;
+            //test to see that the 2 values are the same
+            Assert.AreEqual(AContract.ManufacturerNo, TestData);
+        }
+        [TestMethod]
+        public void StaffNoPropertyOK()
+        {
+            //create some test data to assign to the property
+            Int32 TestData = 1;
+            //assign data to the property
+            AContract.StaffNo = TestData;
+            //test to see that the 2 values are the same
+            Assert.AreEqual(AContract.StaffNo, TestData);
+        }
+        [TestMethod]
+        public void StartDatePropertyOK()
+        {
+            //create some test data to assign to the property
+            DateTime TestData = DateTime.Now.Date;
+            //assign data to the property
+            AContract.StartDate = TestData;
+            //test to see that the 2 values are the same
+            Assert.AreEqual(AContract.StartDate, TestData);
         }
     }
 }
