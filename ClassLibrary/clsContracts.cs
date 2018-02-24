17701febbddc9aec625e0943d5fc483780045f22
maxamillls <morrm046@gmail.com>
2018-02-24 22:51:51 +0000

    Added the test plans for Price Per Month, Duration,

--- ClassLibrary/clsContracts.cs
+++ ClassLibrary/clsContracts.cs
@@ -19,11 +19,19 @@ namespace PhonePalClassLibrary
         public string Duration { get; set; }//public property for Duration - data type string
         public int StaffNo { get; set; }//public property for Staff No - data type int 
         public DateTime StartDate { get; set; }//public property for Start Date - data type string
-        public bool Valid(string SomeContractType, string SomeDataAllowance, string SomeNumberOfMinutes, string SomeNumberOfTexts, int SomePricePerMonth)
+        public bool Valid(string SomeContractType, 
+            string SomeDataAllowance, string SomeNumberOfMinutes, 
+            string SomeNumberOfTexts, int SomePricePerMonth, string 
+            SomeDuration, int SomeContractNo, int 
+            SomeCustomerNo, int SomeManufacturerNo, 
+            int SomeStaffNo, DateTime TheStartDate)
+
         {
+            //create a temporary variable to store data values
+            DateTime DateTemp;
             //boolean flag to indicate that all is ok
             Boolean OK = true;
-            //if the name of the contract type is not blank
+            //if the name of the contract type is blank
             if (SomeContractType == "")
             {
                 //flag an error
@@ -35,46 +43,68 @@ namespace PhonePalClassLibrary
                 //flag an error
                 OK = false;
             }
+            //if data allowance is blank
             if (SomeDataAllowance == "")
             {
                 //flag an error
                 OK = false;
             }
-            //if the name of the contract type is more than 10 characters 
+            //if the name of the data allowance is more than 10 characters 
             if (SomeDataAllowance.Length > 10)
             {
                 //flag an error
                 OK = false;
             }
+            //if number of minutes is blank
             if (SomeNumberOfMinutes == "")
             {
                 //flag an error
                 OK = false;
             }
-            //if the name of the contract type is more than 50 characters 
+            //if the name of number of minutes  is more than 50 characters 
             if (SomeNumberOfMinutes.Length > 50)
             {
                 //flag an error
                 OK = false;
             }
+            //if number of texts is blank
             if (SomeNumberOfTexts == "")
             {
                 //flag an error
                 OK = false;
             }
-            //if the name of the contract type is more than 50 characters 
+            //if the name of Number of Texts is more than 50 characters 
             if (SomeNumberOfTexts.Length > 50)
             {
                 //flag an error
                 OK = false;
             }
+            //if price of month is equal to zero
             if (SomePricePerMonth == 0)
             {
                 //flag an error
                 OK = false;
             }
-            //if the name of the contract type is more than 50 characters 
-            if (SomePricePerMonth > 999999999)
+            //if the number is higher than 9999 for price per month
+            if (SomePricePerMonth > 9999)
+            {
+                //flag an error
+                OK = false;
+            }
+            //if duration is blank
+            if (SomeDuration == "")
+            {
+                //flag an error
+                OK = false;
+            }
+            //if the name of duration is less than 6 characters
+            if (SomeDuration == "")
+            {
+                //flag an error
+                OK = false;
+            }
+            //if the name of duration is more than 11 characters
+            if (SomeDuration.Length > 20)
             {
                 //flag an error
                 OK = false;
@@ -82,5 +112,7 @@ namespace PhonePalClassLibrary
             return OK;
         }
 
+
     }
+    
     }
