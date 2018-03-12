021ed8f2903930b4542547b13caaefa22135a880
p16223017 <p16223017@my365.dmu.ac.uk>
2018-03-12 13:54:04 +0000

    New tests added

--- PhonePalTest/tstCustomer.cs
+++ PhonePalTest/tstCustomer.cs
@@ -24,8 +24,14 @@ namespace PhonePalTest
             clsCustomer ACustomer = new clsCustomer();
             //create some test data to assign to the property
             Boolean TestData = true;
-            //
+            //assign the data to the peroperty
+            ACustomer.Active = TestData;
+            //test to see that the two values are th same
+            Assert.AreEqual(ACustomer.Active, TestData);
         }
+
+        [TestMethod]
+
     
     }
 }
